e6f5345c2bc95483d66433afce8d12c462832736
StopITnowFAST <Bertasdraf@mail.ru>
2022-12-10 10:49:37 +0300

    добавил проверку на размер списка

--- 1.py
+++ 1.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+
 # Ввести список А из 10 элементов, найти наименьший элемент и переставить его с
 # последним элементом. Преобразованный массив вывести.
 
 if __name__ == '__main__':
 
+    N = 10
+
     numbers = [int(a) for a in input().split()]
+    if len(numbers) != N:
+        print("Неправильный размер массива")
+        sys.exit()
 
     print("Изначальный список: ", numbers)
 
